c57438951274d206b71f83e477e93705c2455d1d
mahinhasan <mahinhasanaiub@gmail.com>
2024-09-08 00:51:19 +0600

    Github action added

--- food_delivery/food_delivery/settings.py
+++ food_delivery/food_delivery/settings.py
@@ -84,10 +84,15 @@ WSGI_APPLICATION = 'food_delivery.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'food_delivery',
+        'USER': 'postgres',
+        'PASSWORD': 'Mahin@db',
+        'HOST': 'localhost',  
+        'PORT': '5432',       
     }
 }
+
 
 
 REST_FRAMEWORK = {
